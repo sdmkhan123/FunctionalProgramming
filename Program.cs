0acf458d52e9784a94e956bea5834c8eeded4c28
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-03 16:15:05 +0530

    welcome to Functional Programming

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programming");
+        }
+    }
+}
