cebf0dffdc3aa3aaac96c1f3b99460e3d5d15563
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-03 21:01:54 +0530

    Swap two numbers

--- Program.cs
+++ Program.cs
@@ -4,21 +4,22 @@ namespace FunctionalProgramming
 {
     class Program
     {
-        public static void QuotientAndRemainder(int dividend, int divisor)
+        public static void swapTwoNum(int num1, int num2)
         {
-            int quotient = dividend / divisor;
-            int remainder = dividend % divisor;
-            Console.WriteLine("Quotient = " + quotient);
-            Console.WriteLine("Remainder = " + remainder);
+            Console.WriteLine("Before swapping num1 and num2 are " + num1 + " " + num2);
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("After swapping num1 and num2 are " + num1 + " " + num2);
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Functional Programming");
-            Console.WriteLine("Enter Dividend ");
-            int dividend = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Divisor ");
-            int divisor = Convert.ToInt32(Console.ReadLine());
-            QuotientAndRemainder(dividend, divisor);
+            Console.WriteLine("Enter num2 ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter num1 ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            swapTwoNum(num1, num2);
         }
     }
 }
