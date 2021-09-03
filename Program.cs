1d62785865f0301c1e983c7ae2e878f945e1c32c
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-03 17:12:08 +0530

    Percentage of Head and Tail

--- Program.cs
+++ Program.cs
@@ -4,9 +4,31 @@ namespace FunctionalProgramming
 {
     class Program
     {
+        public const int flipHead = 0;
+        public const int flipTail = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Functional Programming");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int headCount = 0;
+            int tailCount = 0;
+            for (int i=0; i<n; i++)
+            {
+                Random random = new Random();
+                int flipCheck = random.Next(0, 2);
+                if(flipCheck == flipHead)
+                {
+                    headCount++;
+                }
+                else
+                {
+                    tailCount++;
+                }
+            }
+            double headPercentage = 100 * headCount / n;
+            double tailPercentage = 100 * tailCount / n;
+            Console.WriteLine("head percentage is " + headPercentage);
+            Console.WriteLine("tail percentage is " + tailPercentage);
         }
     }
 }
